fb9d7bfda6bdd2842d42d2f8cc22525eab929661
Kunal Mangal <kmangal0991@gmail.com>
2022-08-06 21:06:58 +0530

    Basic outline

--- app.R
+++ app.R
@@ -14,46 +14,36 @@
 
 
 library(shiny)
+library(tidyverse)
+
+# Load data
+df <- readRDS('data/clean/merged_01_2019.Rds')
 
 
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("TNPSC Group 1 Selection Statistics"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
+# Sever logic
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  output$wrote_prelim <- renderText({ 
+    print(input$exservice)
+    sum(df$wrote.prelim)
+  })
+  
+  output$wrote_main <- renderText({ 
+    sum(df$wrote.main)
+  })
+  
+  output$wrote_main <- renderText({ 
+    sum(df$wrote.main)
+  })
+  
+  output$selected <- renderText({ 
+    sum(df$selected)
+  })
+  
 }
 
 # Run the application 
+
+html <- read_file("www/template.html")
+ui <- htmlTemplate(text_ = html)
 shinyApp(ui = ui, server = server)
